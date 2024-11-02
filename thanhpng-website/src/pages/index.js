edfc42c34388154aaff8917fdc87e354348b1516
Thanh Nguyen <thanh.p.nguyen46@gmail.com>
2024-11-01 20:38:23 -0400

    Reformat index.js

--- thanhpng-website/src/pages/index.js
+++ thanhpng-website/src/pages/index.js
@@ -1,54 +1,75 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
-import styles from './index.module.css';
-import SyllabusPage from './_syllabus-page.mdx'
-import MDXContent from '@theme/MDXContent';
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import Heading from "@theme/Heading";
+import styles from "./index.module.css";
+import SyllabusPage from "./_syllabus-page.mdx";
+import MDXContent from "@theme/MDXContent";
 
-function HomepageHeader() {
+export function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Courses 📆
-          </Link>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial 🦖
-          </Link>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <div className="container" style={{ zIndex: 100 }}>
+        <div className={"row"}>
+          <div className={"col"}>
+            <h1 className="hero__title">
+              {siteConfig.customFields.course_number} <br />
+              {siteConfig.title}
+            </h1>
+            <p className="hero__subtitle">
+              {siteConfig.customFields.semester} {siteConfig.tagline}
+            </p>
+            <div className={"col button_group"}>
+              <Link
+                className="button button--secondary button--lg"
+                to="/docs/intro"
+              >
+                Courses 📆
+              </Link>
+              <Link
+                className="button button--secondary button--lg"
+                to="/docs/intro"
+              >
+                Docusaurus Tutorial 🦖
+              </Link>
+            </div>
+          </div>
+
+          {/*{docusaurusConfig.customFields.is_pdf ? <></> : <WatchLiveDemo/>}*/}
         </div>
       </div>
+      {/*<BrowserOnly  fallback={<div>Loading...</div>} >*/}
+      {/*    {() => {*/}
+      {/*        const AnimatedBackground =*/}
+      {/*            require('../components/AnimatedBackground/index').AnimatedBackground;*/}
+      {/*        return <AnimatedBackground/>;*/}
+      {/*    }}*/}
+      {/*</BrowserOnly>*/}
+      <div className="air air1" />
+      <div className="air air2" />
+      <div className="air air3" />
+      <div className="air air4" />
     </header>
   );
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-      <>
-    <Layout
-        title={`Syllabus`}
-        description="Professor Thanh's Syllabus.">
-        <HomepageHeader/>
+    <>
+      <Layout title={`Syllabus`} description="Professor Thanh's Syllabus.">
+        <HomepageHeader />
         <main>
-            <div style={{zIndex:100000}}>
-                <MDXContent>
-                    <SyllabusPage/>
-                </MDXContent>
-            </div>
+          <div style={{ zIndex: 100000 }}>
+            <MDXContent>
+              <SyllabusPage />
+            </MDXContent>
+          </div>
         </main>
-    </Layout></>
+      </Layout>
+    </>
   );
 }
